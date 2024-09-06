d86ac62c83af20f0fbac9c570e3ce53a3355d288
artxofxwords <brennan.amanda.j@gmail.com>
2024-09-05 23:27:11 -0400

    fixes

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-mongoose.connect(`mongodb+srv://amandab:Atn!AS2024@portfoliofeedback.me0jj.mongodb.net/PORTFOLIOFEEDBACK`);
+mongoose.connect("mongodb+srv://amandab:Atn!AS2024@portfoliofeedback.me0jj.mongodb.net/PORTFOLIOFEEDBACK");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error."));
