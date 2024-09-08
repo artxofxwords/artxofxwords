dd340fb71f43498f620c12ed0fd5d8ea88c1bd9f
artxofxwords <brennan.amanda.j@gmail.com>
2024-09-08 07:23:06 -0400

    cors adjust

--- client/src/components/Footer/FeedbackForm.jsx
+++ client/src/components/Footer/FeedbackForm.jsx
@@ -46,6 +46,7 @@ export default function FeedbackForm() {
       body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
       },
     });
 
--- server/app.js
+++ server/app.js
@@ -8,16 +8,16 @@ const feedbackRoute = require("./routes/feedbackRoute.js");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 
-const corsOptions = {
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-}
+// const corsOptions = {
+//     origin:'*', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+// }
 
-//app.use(cors());
-app.use(cors(corsOptions));
+app.use(cors());
+// app.use(cors(corsOptions));
 app.use("/feedback", feedbackRoute);
 
 mongoose.connect(process.env.MONGO_URI);
