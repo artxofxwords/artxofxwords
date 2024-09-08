966b5eb837eac42726e379ab518e79df8f2a8367
artxofxwords <brennan.amanda.j@gmail.com>
2024-09-08 06:32:35 -0400

    urlencoded

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const feedbackRoute = require("./routes/feedbackRoute.js");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI);
