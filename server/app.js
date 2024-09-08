41869dfaa1885e4d2623166d4c593315b8bca646
artxofxwords <brennan.amanda.j@gmail.com>
2024-09-08 06:40:52 -0400

    cors update

--- server/app.js
+++ server/app.js
@@ -8,8 +8,16 @@ const feedbackRoute = require("./routes/feedbackRoute.js");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
+const corsOptions = {
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+//app.use(cors());
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.MONGO_URI);
 
